1813bb23a2be6cb110f6327cc5f369e411495e6d
vagrant <vagrant@localhost.localdomain>
2020-11-03 12:37:16 +0900

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,76 @@
+class UsersController < ApplicationController
+  # before_action :require_user_logged_in, only: [:index, :show, :edit, :update, :destroy, :followings, :followers, :likes]
+  
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(10)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # @cats = @user.cats.order(id: :desc).page(params[:page]).per(6)
+    # counts(@user)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      flash[:success] = 'ユーザの更新に成功しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの更新に失敗しました。'
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:success] = 'ユーザは正常に削除されました。'
+    redirect_to root_url
+  end
+  
+  # def followings
+  #   @user = User.find(params[:id])
+  #   @followings = @user.followings.page(params[:page]).per(6)
+  #   counts(@user)
+  # end
+  
+  # def followers
+  #   @user = User.find(params[:id])
+  #   @followers = @user.followers.page(params[:page]).per(6)
+  #   counts(@user)
+  # end
+  
+  # def likes
+  #   @user = User.find(params[:id])
+  #   @likes = @user.likes.page(params[:page]).per(6)
+  #   counts(@user)
+  # end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  # validates :profile, presence: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  
+  # has_many :cats, dependent: :destroy
+  # has_many :relationships, dependent: :destroy
+  # has_many :followings, through: :relationships, source: :follow, dependent: :destroy
+  # has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
+  # has_many :followers, through: :reverses_of_relationship, source: :user, dependent: :destroy
+  
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+  
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+  
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+  
+  # 自分がフォローしたユーザの投稿もにゃんこ一覧に表示させるために必要。
+  def feed_cats
+    Cat.where(user_id: self.following_ids + [self.id])
+  end
+  
+  # has_many :favorites, dependent: :destroy
+  # has_many :likes, through: :favorites, source: :cat, dependent: :destroy
+  
+  def favorite(cat)
+    self.favorites.find_or_create_by(cat_id: cat.id)
+  end
+  
+  def unfavorite(cat)
+    favorite = self.favorites.find_by(cat_id: cat.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorited?(cat)
+    self.likes.include?(cat)
+  end
+end
