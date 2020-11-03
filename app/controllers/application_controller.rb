2ca7fdacbb42e72bd6a1fa154ffea598f597b6f2
vagrant <vagrant@localhost.localdomain>
2020-11-03 13:49:44 +0900

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  
+  include SessionsHelper
+  
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  def counts(user)
+    @count_cats = user.cats.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+    @count_likes = user.likes.count
+  end
 end
